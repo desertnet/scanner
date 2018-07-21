a22eabd5ad26fcd4ae421148c3ba4ec7fa901a33
Eric McCarthy <eric@limulus.net>
2018-07-21 10:41:59 -0700

    Simplify index.js, and stop exporting Scanner module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import Scanner from './lib/Scanner'
-import Token from './lib/Token'
-import Dialect from './lib/Dialect'
-import TokenDefinition from './lib/TokenDefinition'
-import BufferedTokenizer from './lib/BufferedTokenizer'
-
-module.exports = Scanner
-module.exports.Token = Token
-module.exports.Dialect = Dialect
-module.exports.TokenDefinition = TokenDefinition
-module.exports.BufferedTokenizer = BufferedTokenizer
+export {default as Token} from './lib/Token'
+export {default as Dialect} from './lib/Dialect'
+export {default as TokenDefinition} from './lib/TokenDefinition'
+export {default as BufferedTokenizer} from './lib/BufferedTokenizer'
