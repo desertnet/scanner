d83cc4b1eeba8688cece2e60b5e7f5e17c3e9e04
Eric McCarthy <eric@limulus.net>
2018-07-22 13:17:57 -0700

    Make Dialect class immutable

--- src/lib/Dialect.js
+++ src/lib/Dialect.js
@@ -3,16 +3,27 @@ import TokenDefinition from './TokenDefinition'
 const tokenDefsSym = Symbol('tokensDef')
 
 export default class Dialect {
-  constructor (identifier) {
-    this.identifier = identifier
-    this[tokenDefsSym] = new Set()
-  }
-
-  addTokenDefinition (definition) {
-    if (!(definition instanceof TokenDefinition)) {
-      throw new TypeError(`Expected TokenDefinition`)
+  constructor (identifier, tokenDefs) {
+    if (identifier === undefined) {
+      throw new TypeError(`Expected identifier`)
     }
-    this[tokenDefsSym].add(definition)
+
+    if (!Array.isArray(tokenDefs)) {
+      throw new TypeError(`Expected array of token definitions`)
+    }
+
+    if (tokenDefs.length === 0) {
+      throw new TypeError(`Expected non-empty array of token definitions`)
+    }
+
+    for (const tokenDef of tokenDefs) {
+      if (!(tokenDef instanceof TokenDefinition)) {
+        throw new TypeError(`Expected TokenDefinition`)
+      }
+    }
+
+    this.identifier = identifier
+    this[tokenDefsSym] = new Set(tokenDefs)
   }
 
   get tokenDefinitions () {
