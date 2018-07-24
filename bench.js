2425e7f0f9710b7ea7e51a978a4cb9a67fc2fe8d
Eric McCarthy <eric@limulus.net>
2018-07-23 21:10:25 -0700

    Benchmark line number tree build

--- bench.js
+++ bench.js
@@ -4,7 +4,7 @@ const {BufferedScanner, Dialect, TokenDefinition} = require('./dist')
 // import {BufferedScanner, Dialect, TokenDefinition} from './src'
 
 const length = 100000
-const subject = Array(length).fill(`if (this) then {that}`, 0, length).join(' ')
+const subject = Array(length).fill(`if (this) then {that}`, 0, length).join('\n')
 
 const myLang = new Dialect('myLang', [
   new TokenDefinition('IF', String.raw`if`),
@@ -18,7 +18,7 @@ const myLang = new Dialect('myLang', [
   new TokenDefinition('VAR', String.raw`\w+`),
 ])
 
-const scanner = new BufferedScanner(subject)
+let scanner = new BufferedScanner(subject)
 
 console.log(`scanning ${Math.floor(subject.length / 1024)}KB equivalent file`)
 
@@ -26,6 +26,23 @@ const t0 = Date.now()
 for (const _tok of scanner.generateTokensUsingDialect(myLang)) {
   /* nada */
 }
-console.log(`${Date.now() - t0}ms`)
+console.log(`raw scan: ${Date.now() - t0}ms`)
+
+const tokens = []
+scanner = new BufferedScanner(subject)
+const t1 = Date.now()
+for (const tok of scanner.generateTokensUsingDialect(myLang)) {
+  tokens.push(tok)
+}
+console.log(`scan: ${Date.now() - t1}ms`)
+
+const t2 = Date.now()
+tokens[Math.floor(length / 2)].line
+console.log(`first lineno: ${Date.now() - t2}ms`)
+
+const t3 = Date.now()
+tokens[Math.floor(length / 2)].line
+console.log(`second lineno: ${Date.now() - t3}ms`)
+
 
 /* eslint no-console: off */
